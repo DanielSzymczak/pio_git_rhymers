8a68c026affa1938ef59ce6e5b491a35f62b6dae
DanielSzymczak <245941@edu.p.lodz.pl>
2024-07-01 13:36:35 +0200

    9. Zmiana widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,22 +2,22 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
-    public static final int EMPTY = -1;
+    private static final int SIZE = 12;
+    private static final int EMPTY = -1;
     private final int[] numbers = new int[SIZE];
 
     public int total = EMPTY;
 
-    public void countIn(int in) {
+    protected void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-   public boolean callCheck() {
+   protected boolean callCheck() {
         return total == EMPTY;
    }
 
-   public boolean isFull() {
+   protected boolean isFull() {
 
         return total == SIZE-1;
    }
@@ -28,7 +28,7 @@ public class DefaultCountingOutRhymer {
         return numbers[total];
    }
 
-    public int countOut() {
+    protected int countOut() {
         if (callCheck())
             return EMPTY;
         return numbers[total--];
